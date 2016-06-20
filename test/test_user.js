a1fff72ce0b68bd7de6883d22d5307c69223e330
Jolaade Adewale <jolaade.adewale@andela.com>
2016-06-20 23:41:27 +0100

    pass test case except role update

---
+++ test/test_user.js
@@ -0,0 +1,101 @@
+'use strict';
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server');
+var should = chai.should();
+var token1 = '';
+var token2 = '';
+
+chai.use(chaiHttp);
+
+describe('users', function() {
+  it('should create a new user on /users POST', function(done) {
+  chai.request(server)
+    .post('/users/')
+    .send({'username':'Tope', 'password':'Tope',
+      'firstName':'tope', 'lastName':'Fowotade',
+      'email':'tope@yahoo.com','role':'Administrator'})
+    .end(function(err, res){
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('message');
+      res.body.should.have.property('token');
+      token1 = res.body.token;
+      res.body.message.should.equal('user created');
+      done();
+    });
+});
+ it('should fail because visitor role is not defined on /users POST', function (done) {
+    chai.request(server)
+    .post('/users/')
+    .send({'username':'Joliphizzle', 'password':'Jolaade',
+      'firstName':'Jolaade', 'lastName':'Adewale',
+      'email':'jbadewale@yahoo.com', 'role':'visitor'})
+    .end(function(err, res){
+      res.should.have.status(409);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('error');
+      res.body.error.should.equal('undefined role');
+      done();
+    });
+  });
+
+  it('should add user to system with a defined role', function (done) {
+    chai.request(server)
+    .post('/users/')
+    .send({'username':'Joliphizzle', 'password':'Jolaade',
+      'firstName':'Jolaade', 'lastName':'Adewale',
+      'email':'jbadewale@yahoo.com', 'role':'Guest'})
+    .end(function(err, res){
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('message');
+      res.body.should.have.property('token');
+      token2 = res.body.token;
+      res.body.message.should.equal('user created');
+      done();
+    });
+
+  });
+  it('should fail because token not provided /users/ GET', function (done) {
+    chai.request(server)
+    .get('/users/')
+    .end(function(err, res){
+      res.should.have.status(403);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('message');
+      res.body.success.should.equal(false);
+      res.body.message.should.equal('No token provided.');
+      done();
+    });
+  });
+  it('should log user in /users/login/ POST', function (done) {
+    chai.request(server)
+    .post('/users/login')
+    .send({'username':'Joliphizzle', 'password':'Jolaade'})
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('token');
+      token1 = res.body.token;
+      done();
+    });
+  });
+  it('should return all users /users/ GET', function (done) {
+    chai.request(server)
+    .get('/users/?token='+token1)
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      done();
+    });
+  });
+});
+
+
---
+++ test/test_user_documents.js
@@ -0,0 +1,104 @@
+'use strict';
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server');
+var should = chai.should();
+var id = '';
+
+
+chai.use(chaiHttp);
+
+describe('documents', function() {
+  it('should create a new document', function(done) {
+     chai.request(server)
+    .post('/users/login')
+    .send({'username':'Joliphizzle', 'password':'Jolaade'})
+    .end(function(err, res){
+    chai.request(server)
+    .post('/documents/?token='+res.body.token)
+    .send({'title':'Test', 'id' : res.body.data._id, 'content' :
+      'This is javacsript'})
+    .end(function(err, res){
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('status');
+      res.body.should.have.property('document');
+      res.body.status.should.equal('document saved');
+      res.body.document.should.have.property('createdAt');
+      done();
+    });
+  });
+});
+  it('should return documents with limit', function (done) {
+     chai.request(server)
+    .post('/users/login')
+    .send({'username':'Joliphizzle', 'password':'Jolaade'})
+    .end(function(err, res) {
+    chai.request(server)
+    .get('/documents/?token='+res.body.token+'&limit='+'10')
+    .end(function(err, res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('data');
+      res.body.data.should.be.a('array');
+      //console.log(res.body.data[0]._id);
+      id = res.body.data[0]._id;
+     // res.body.data.length.be.at.most(10);
+      done();
+    });
+    });
+  });
+
+  it('should update test document', function (done) {
+    chai.request(server)
+    .post('/users/login')
+    .send({'username':'Tope', 'password':'Tope'})
+    .end(function(err, res){
+    chai.request(server)
+    .put('/documents/'+id+'/?token='+res.body.token)
+    .send({'role':['Administrator','Guest']})
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      console.log(res);
+      res.body.success.access.should.equal(['Administrator','Guest']);
+      done();
+    });
+    });
+  });
+  it('returns documents accessed by defined role', function (done) {
+     chai.request(server)
+    .post('/users/login')
+    .send({'username':'Tope', 'password':'Tope'})
+    .end(function(err, res){
+    chai.request(server)
+    .get('/documents/?token=' + res.body.token +
+      '&role=' + res.body.data.role + '&limit=10')
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      //console.log(res);
+      done();
+    });
+    });
+  });
+   it('returns documents created on a particular date', function (done) {
+    chai.request(server)
+    .post('/users/login')
+    .send({'username':'Tope', 'password':'Tope'})
+    .end(function(err, res){
+    chai.request(server)
+    .get('/documents/?token=' + res.body.token +
+      '&date=' + new Date() + '&limit=10')
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      //console.log('test');
+     // console.log(res);
+      done();
+    });
+  });
+});
+  });
