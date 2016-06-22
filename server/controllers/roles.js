165dc1633728e46e90ef2ec482967b79c050b0ee
Jolaade Adewale <jolaade.adewale@andela.com>
2016-06-22 09:44:20 +0100

    remove getrole function

--- server/controllers/roles.js
+++ server/controllers/roles.js
@@ -5,6 +5,7 @@
   module.exports = (function(app, Schema, db) {
     var model = require('../model/role_model.json'),
       validateRole = require('../services/validate_roles'),
+      responsehelper  = require('../services/response_helper'),
       roleSchema = new Schema(model).plugin(db.autoIncrement.plugin, 'Role'),
       Roles = db.connection.model('Role', roleSchema);
       app.set('roleModel',Roles);
@@ -13,38 +14,24 @@
       if(validateRole.validRoleCreation(req)){
         var addRole = new Roles(validateRole.parseData(req));
           addRole.save(function(err, role) {
-            if (err){
-              res.status(409).json({
-              error: err.message || err.errors[0].message
+            if (err) {
+              responsehelper.response(res, 409, {
+                error: err.message || err.errors[0].message
               });
             }
             else{
-              res.status(201).json({
+              responsehelper.response(res, 201, {
                 status: role
               });
             }
           });
       }
       else{
-        res.status(409).json({
+        responsehelper.response(res, 409, {
           error: 'check manual for required params'
         });
       }
     };
-
-    var getRole = function (role, res) {
-      Roles.findOne(role, function (err, roles) {
-        if (err){
-          res.status(500).json({
-          error: err.message || err.errors[0].message
-          });
-        }
-        else{
-            res(roles);
-          }
-        });
-      };
-
 
     var getRoles = function (req, res) {
       Roles.find(function (err, roles) {
@@ -68,7 +55,6 @@
 
     return {
       createRole : createRole,
-      getRole : getRole,
       getRoles : getRoles
     };
 
