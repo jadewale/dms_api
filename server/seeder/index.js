84f09421ec097a52dec453ba43230175df95235f
Jolaade Adewale <jolaade.adewale@andela.com>
2016-06-23 11:07:13 +0100

    clear all collections

--- server/seeder/index.js
+++ server/seeder/index.js
@@ -2,27 +2,34 @@
 (function () {
  'use strict';
  var db = require('mongoose'),
-   model = require('../model/role_model.json'),
+   role = require('../model/role_model.json'),
+   user = require('../model/user_model.json'),
+   doc = require('../model/documents_model.json'),
    object = [{'title': 'Administrator', 'id' : 1},
    {'title': 'User', 'id' : 2},
    {'title': 'Guest', 'id' : 3}];
 
-   var options = {
+
+ var options = {
       server : { poolSize : 5, reconnectTries : 5},
       user : 'USER',
       pass : ''
-    };
+  };
    var uri = 'mongodb://localhost:27017/DocumentManagementSystem';
     db.connect(uri,options);
     db.connection.on('connected', function () {
       console.log('Default connection open');
     });
     var Schema = db.Schema;
-    var Roles = db.model('Role', new Schema(model));
+    var Roles = db.model('Role', new Schema(role));
+    var User = db.model('User', new Schema(user));
+    var Doc = db.model('Documents', new Schema(doc));
 
    Roles.remove({}, function (err, role) {
-    if(err)
-    console.log(err);
+   });
+   User.remove({}, function (err, role) {
+   });
+   Doc.remove({}, function (err, role) {
    });
    object.forEach(function (obj, index) {
 
