7ad745abb3a16b3101989e355c910b62fc5ba1b6
Jolaade Adewale <jolaade.adewale@andela.com>
2016-06-25 15:28:57 +0100

    fix indentation issue

--- server/seeder/index.js
+++ server/seeder/index.js
@@ -1,7 +1,8 @@
 
 (function () {
  'use strict';
- var db = require('mongoose'),
+
+  var db = require('mongoose'),
    role = require('../model/role_model.json'),
    user = require('../model/user_model.json'),
    doc = require('../model/documents_model.json'),
@@ -10,40 +11,39 @@
    {'title': 'Guest', 'id' : 3}];
 
 
- var options = {
+  var options = {
       server : { poolSize : 5, reconnectTries : 5},
       user : 'USER',
       pass : ''
   };
 
-   var uri = 'mongodb://localhost:27017/DocumentManagementSystem';
-    db.connect(uri,options);
-    db.connection.on('connected', function () {
-      console.log('Default connection open');
-    });
-    var Schema = db.Schema;
-    var Roles = db.model('Role', new Schema(role));
-    var User = db.model('User', new Schema(user));
-    var Documents = db.model('Documents', new Schema(doc));
+  var uri = 'mongodb://localhost:27017/DocumentManagementSystem';
+  db.connect(uri,options);
+  db.connection.on('connected', function () {
+    console.log('Default connection open');
+  });
+  var Schema = db.Schema;
+  var Roles = db.model('Role', new Schema(role));
+  var User = db.model('User', new Schema(user));
+  var Documents = db.model('Documents', new Schema(doc));
 
-   // Empty roles collection
-   Roles.remove({}, function (err, role) {
-   });
+  // Empty roles collection
+  Roles.remove({}, function (err, role) {
+  });
 
-   // Empty user collection
-   User.remove({}, function (err, rol) {
-   });
+  // Empty user collection
+  User.remove({}, function (err, rol) {
+  });
 
-   // Empty documents collection
-   Documents.remove({}, function (err, doc) {
+  // Empty documents collection
+  Documents.remove({}, function (err, doc) {
     object.forEach(function (obj, index) {
 
      var addRole = new Roles(obj);
      addRole.save(function (err, role) {
        err ? console.log(err) : (index === 2) ? process.exit(0): false;
      });
-
-   });
-   });
+    });
+  });
 
 }());
